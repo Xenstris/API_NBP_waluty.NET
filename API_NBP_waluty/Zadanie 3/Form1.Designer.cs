e44819fdf6c238e46cd14eaeb482dd0f963bf4c2
Xenstris <90719297+Xenstris@users.noreply.github.com>
2022-04-06 13:01:54 +0200

    Add files via upload

---
+++ API_NBP_waluty/Zadanie 3/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Zadanie_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 341);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(422, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(465, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Uśrednij";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(414, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Podaj walute do uśrednienia ( format bez .txt)";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(169, 171);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(208, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ API_NBP_waluty/Zadanie 3/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+using System.Globalization;
+
+
+namespace Zadanie_3
+{
+    public partial class Form1 : Form
+    {
+
+        internal static Form2 form2;
+        internal static Form1 form1;
+
+
+        public static List<string> lista = new List<string>();
+
+        public Form1()
+        {
+            new JObject();
+            InitializeComponent();
+            form1 = this;
+            timer1.Interval = 30000;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            form2 = new Form2(this);
+            form2.Show();
+        }
+
+        public void add_waluta (string waluta)
+        {
+            try
+            {
+                string url = "http://api.nbp.pl/api/exchangerates/rates/a/" + waluta + "/?format=json";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                var status = response.StatusCode;
+                string content = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                dynamic date = JObject.Parse(content);
+
+                bool jest = false;
+
+                foreach (string element in lista)
+                {
+                    if(element == waluta)
+                    {
+                        jest = true;
+                        MessageBox.Show("Waluta znajduje się już na liście");
+                    }
+                }
+
+                if (jest == false)
+                {
+                    dodaj(waluta, (string)date.rates[0].effectiveDate, (string)date.rates[0].mid);
+                }
+
+            }
+            catch(Exception zmienna)
+            {
+
+                MessageBox.Show(zmienna.Message);
+
+            }
+
+
+        }
+
+        private void dodaj (string curr, string data, string kurs)
+        {
+            lista.Add(curr);
+            string name = "Currency/" + curr + ".txt";
+            DateTime date1 = DateTime.Now;
+            using (StreamWriter save = new StreamWriter(name, true))
+            {
+                save.WriteLine(curr + " | " + data + " | " + kurs + " | " + date1);
+            }
+            
+        }
+
+         public void show_lista()
+        {
+            textBox1.Text = "";
+            foreach (string element in lista)
+            {
+                textBox1.Text += element + ", ";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for(int i=0; i<lista.Count(); i++)
+            {
+                string url = "http://api.nbp.pl/api/exchangerates/rates/a/" + lista[i] + "/?format=json";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                var status = response.StatusCode;
+                string content = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                dynamic date = JObject.Parse(content);
+
+                string name = "Currency/" + lista[i] + ".txt";
+                string curr = lista[i];
+                string data = (string)date.rates[0].effectiveDate;
+                string kurs = (string)date.rates[0].mid;
+
+                DateTime date1 = DateTime.Now;
+                using (StreamWriter save = new StreamWriter(name, true))
+                {
+                    save.WriteLine(curr + " | " + data + " | " + kurs + " | " + date1);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = textBox2.Text;
+                textBox3.Text = read(path).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Błędna waluta");
+            }
+
+            
+        }
+
+        private double read (string path)
+        {
+            double avr = 0;
+            int amount = 0;
+
+            using (StreamReader sr = new StreamReader("Currency/" + path + ".txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (!(line == ""))
+                    {
+                        string[] red = line.Split('|');
+                        double result = double.Parse(red[2], CultureInfo.InvariantCulture);
+                        avr += result;
+                        amount++;
+                    }
+                }
+            }
+            return (avr / amount);
+        }
+
+    }
+
+
+}
+ 
